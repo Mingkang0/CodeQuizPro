756ce15c0e094f23d19f3d0393afe8ddc8aa788f
Mingkang0 <98515527+Mingkang0@users.noreply.github.com>
2023-09-08 14:33:28 +0800

    Open quiz.tsx .css and change div header to ion-header

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,7 +1,7 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonLabel } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
-import { IonIcon } from '@ionic/react';
+import { IonIcon, IonButton, IonButtons } from '@ionic/react';
 import { menuOutline, personCircleOutline, bookOutline } from 'ionicons/icons';
 import { IonSearchbar } from '@ionic/react';
 import { IonCard, IonCardContent, IonCardTitle } from '@ionic/react';
@@ -12,25 +12,34 @@ import challenges from '../assests/edit.png'
 const Home: React.FC = () => {
   return (
     <IonPage>
+      <IonHeader className='Header'>
+        <IonToolbar className='header'>
+          <IonButtons slot="start">
+            <IonButton className='custom-button'>
+              <IonIcon icon={menuOutline} className='custom-icon' />
+            </IonButton>
+          </IonButtons>
+          <IonLabel className='header-text' style={{textAlign:'center'}}>CodeQuiz Pro</IonLabel>
+          <IonButtons slot="end">
 
-      <IonContent style={{'--ion-background-color':'antiquewhite'}} fullscreen>
+            <IonButton className='custom-button'>
+              <IonIcon icon={personCircleOutline} className='custom-icon' />
+            </IonButton>
+          </IonButtons>
+        </IonToolbar>
+      </IonHeader>
+
+      <IonContent style={{ '--ion-background-color': 'antiquewhite' }} fullscreen>
         <link href="https://fonts.googleapis.com/css?family=Inria+Sans" rel="stylesheet" />
-        
-        <div className="header">
-          <IonIcon icon={menuOutline} className="custom-icon" />
 
-          <IonLabel className='header-text'>CodeQuiz Pro</IonLabel>
 
-          <IonIcon icon={personCircleOutline} className='custom-icon' />
-
-        </div>
         <div className='content'>
           <div className='search-container'>
             <IonSearchbar className='searchbar'></IonSearchbar>
           </div>
           <div className='quiz'>
             <IonCard className="card-button">
-              <IonCardContent className="card-content" style={{backgroundColor: '#ffbfbf'}}>
+              <IonCardContent className="card-content" style={{ backgroundColor: '#ffbfbf' }}>
                 <div className="left-content">
                   <IonCardTitle className="card-title">Quizzes</IonCardTitle>
                   <div className="card-text">Test your knowledge</div>
@@ -42,7 +51,7 @@ const Home: React.FC = () => {
             </IonCard>
           </div>
           <div className='challenges'>
-            <IonCard className="card-button" style={{backgroundColor: '#ccfcc2', marginTop:'30px'}}>
+            <IonCard className="card-button" style={{ backgroundColor: '#ccfcc2', marginTop: '30px' }}>
               <IonCardContent className="card-content">
                 <div className="left-content">
                   <IonCardTitle className="card-title">Daily Challenges</IonCardTitle>
@@ -55,7 +64,7 @@ const Home: React.FC = () => {
             </IonCard>
           </div>
           <div className='learning'>
-            <IonCard className="card-button" style={{backgroundColor: '#fff4cc'}}>
+            <IonCard className="card-button" style={{ backgroundColor: '#fff4cc' }}>
               <IonCardContent className="card-content">
                 <div className="left-content">
                   <IonCardTitle className="card-title">Learning Resources</IonCardTitle>
